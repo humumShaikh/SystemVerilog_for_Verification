6c3aa5d60fcc22ec5139d931fcfa01c7c642d263
HUMUM <humumshaikh0@gmail.com>
2025-12-13 00:55:47 +0530

    Add files via upload

---
+++ Full_Adder_v2/monitor.sv
@@ -0,0 +1,27 @@
+class Monitor;
+
+  mailbox mon2scb;
+  virtual intf vif;
+  
+  function new(virtual intf vif , mailbox mon2scb);
+    this.mon2scb = mon2scb;
+    this.vif = vif;
+  endfunction
+  
+  
+  task main();
+    repeat(40)
+    begin
+      Transaction trans;
+      trans = new();
+      trans.a = vif.a;
+      trans.b = vif.b;
+      trans.c = vif.c;
+      trans.sum = vif.sum;
+      trans.carry = vif.carry;
+      mon2scb.put(trans);
+      trans.display("Monitor Level Signals");
+    end
+  endtask
+
+endclass
