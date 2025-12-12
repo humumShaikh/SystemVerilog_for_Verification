93371353c8d377f2b5a976a5e83778817cc7b802
HUMUM <humumshaikh0@gmail.com>
2025-12-13 00:56:35 +0530

    Add files via upload

---
+++ Full_Adder_v2/fullAdder.sv
@@ -0,0 +1,13 @@
+module fullAdder
+(
+  input wire a,
+  input wire b,
+  input wire c,
+  output wire sum,
+  output wire carry
+);
+
+  assign sum = a ^ b ^ c;
+  assign carry = (a & b) | ((a ^ b) & c);
+  
+endmodule
