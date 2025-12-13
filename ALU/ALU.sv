43c26a659571feef21bec59f9e451c1c1d3675e3
HUMUM <humumshaikh0@gmail.com>
2025-12-14 00:04:41 +0530

    Add files via upload

---
+++ ALU/ALU.sv
@@ -0,0 +1,24 @@
+module ALU
+(
+  input wire A,
+  input wire B,
+  input wire [1:0] S,
+  output wire Y
+);
+
+  logic f;
+  
+  always_comb
+  begin
+    case (S)
+      2'b00 : f = A ^ B;
+      2'b01 : f = A & B;
+      2'b10 : f = A + B;
+      2'b11 : f = A - B;
+    endcase
+  end
+  
+  assign Y = f;
+
+
+endmodule
