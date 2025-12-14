c7fa2459db1bcc5777e363f5352be8c7e8fb52d0
HUMUM <humumshaikh0@gmail.com>
2025-12-14 09:10:31 +0530

    Enhance comments for clarity in driver.sv
    Added comments to clarify the purpose of variables and methods.

--- ALU/driver.sv
+++ ALU/driver.sv
@@ -1,22 +1,25 @@
+//  Driver Class
+//  Drives the transaction object signals to the interface to pass on to the DUT
+
 class Driver;
 
-  mailbox gen2drv;
-  virtual intf vif;
+  mailbox gen2drv;      //mailbox for receiving the transaction object from generator
+  virtual intf vif;    //virtual interface instance
   
-  function new(virtual intf vif , mailbox gen2drv);
+  function new(virtual intf vif , mailbox gen2drv);    //constructor
     this.vif = vif;
     this.gen2drv = gen2drv;
   endfunction
   
   
-  task main();
+  task main();    //main task / responsibility
     repeat(40)
     begin
-      Transaction trans;
-      trans = new();
-      gen2drv.get(trans);
+      Transaction trans;      //handle of object Transaction
+      trans = new();          //allotting memory to it
+      gen2drv.get(trans);      //inbuilt mailbox method to fetch
       
-      vif.A <= trans.A;
+      vif.A <= trans.A;        //Driving 
       vif.B <= trans.B;
       vif.S <= trans.S;
       #10;
