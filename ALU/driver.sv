d8731203f138c0e58bb2c693d7f4902632a27420
HUMUM <humumshaikh0@gmail.com>
2025-12-14 09:11:12 +0530

    Update driver.sv

--- ALU/driver.sv
+++ ALU/driver.sv
@@ -13,18 +13,18 @@ class Driver;
   
   
   task main();    //main task / responsibility
-    repeat(40)
+    repeat(40)    //repeat task 40 times
     begin
       Transaction trans;      //handle of object Transaction
       trans = new();          //allotting memory to it
       gen2drv.get(trans);      //inbuilt mailbox method to fetch
       
-      vif.A <= trans.A;        //Driving 
-      vif.B <= trans.B;
-      vif.S <= trans.S;
+      vif.A <= trans.A;        //Driving the signals to the interface
+      vif.B <= trans.B;        //Driving the signals to the interface
+      vif.S <= trans.S;        //Driving the signals to the interface
       #10;
       
-      trans.display("Driver Class Signals");
+      trans.display("Driver Class Signals");      //transaction class method (optional)
       
     end
   endtask
