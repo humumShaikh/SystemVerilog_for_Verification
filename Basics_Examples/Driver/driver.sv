ff7a9ca0e17d2d8430ffae2b48326b760e00d93a
HUMUM <humumshaikh0@gmail.com>
2025-12-20 22:32:39 +0530

    Update driver.sv

--- Basics_Examples/Driver/driver.sv
+++ Basics_Examples/Driver/driver.sv
@@ -8,7 +8,7 @@ class Driver;
   function new();                                    //This is a constructor that is necessary to be called with each class object in SV or else the memory is not allocated
     $display("Driver Class Object Created");
   endfunction
-  
+                                                        //note that the interface will always be passed as virtual in the task 
   task main(virtual intf vif);                          //takes an interface as arguement, we'll pass the interface that is connecting the dut to the testbench
     $display("Driving Signals through the driver now");  //that interface will then be used 
     vif.A <= 0; vif.B <= 0;
