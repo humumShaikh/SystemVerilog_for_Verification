c6540897eeb0f08dc69c51326133ca9fec6b35f0
HUMUM <humumshaikh0@gmail.com>
2025-12-20 20:39:37 +0530

    Update interface.sv

--- Basics_Examples/Interface/Basic_Interface/interface.sv
+++ Basics_Examples/Interface/Basic_Interface/interface.sv
@@ -1,6 +1,10 @@
+//Absolutely Basic Interface
+//Just define all your DUT ports here , in our case it's a simple 2 input OR Gate
+//Declare the ports as type 'logic'
+
 interface intf();
 
-  logic A;
+  logic A;    
   logic B;
   logic Y;
 
