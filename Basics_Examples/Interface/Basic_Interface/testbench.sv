b24ab39acfd829ddb6ff37e5978c72ca6dd9d2ad
HUMUM <humumshaikh0@gmail.com>
2025-12-20 20:42:43 +0530

    Update testbench.sv

--- Basics_Examples/Interface/Basic_Interface/testbench.sv
+++ Basics_Examples/Interface/Basic_Interface/testbench.sv
@@ -1,19 +1,20 @@
+//Absolutely Simple Testbench Module
+//Unlike traditional verilog testbenches, the testbench here will not have it's own pins for driving the stimulus
+//The DUT that we call here is only supposed to be driven through the interface
+//This example will familiarize and give you an idea of how to give stimulus through the interface
+
 module testbench();
   
-  reg A;
-  reg B;
-  wire Y;
+  intf vif();    //making a static object of interface and calling it as 'vif' ....the round brackets are mandatory here 
   
-  intf vif();
-  
-  orGate OG (
+  orGate OG (    //making a DUT and connecting it's ports to the interface's ports
     .A(vif.A),
     .B(vif.B),
     .Y(vif.Y)
   );
   
   initial
-  begin
+  begin                                                            //simple block to give stimulus, note the interface signals are being manipulated here
     $display("OR-GATE Testbench with a simple interface");
     $monitor("A = %0b B = %0b Y = %0b" , vif.A , vif.B , vif.Y);
     vif.A <= 0; vif.B <= 0;
