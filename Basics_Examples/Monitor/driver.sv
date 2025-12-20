40ab2fc4aa653ef705e4e557c128b73c5d570976
HUMUM <humumshaikh0@gmail.com>
2025-12-20 22:48:00 +0530

    Update driver.sv

--- Basics_Examples/Monitor/driver.sv
+++ Basics_Examples/Monitor/driver.sv
@@ -1,11 +1,16 @@
+//Basic Driver Class
+//The main aim is to not drive the signals in the testbench file, rather than that we will create a drive class
+//Then make an object of this class and define some funcitons of this class i.e. driving the signals 
+//Here it is referred to as 'main'
+
 class Driver;
   
-  function new();
+  function new();                                    //This is a constructor that is necessary to be called with each class object in SV or else the memory is not allocated
     $display("Driver Class Object Created");
   endfunction
-  
-  task main(virtual intf vif);
-    $display("Driving Signals through the driver now");
+                                                        //note that the interface will always be passed as virtual in the task 
+  task main(virtual intf vif);                          //takes an interface as arguement, we'll pass the interface that is connecting the dut to the testbench
+    $display("Driving Signals through the driver now");  //that interface will then be used 
     vif.A <= 0; vif.B <= 0;
     #5;
     vif.A <= 0; vif.B <= 1;
