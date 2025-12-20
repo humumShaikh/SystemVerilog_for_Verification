6eb73b421aab806509c4f25ec200cab6cbbc27e6
HUMUM <humumshaikh0@gmail.com>
2025-12-20 22:27:26 +0530

    Update testbench.sv

--- Basics_Examples/Driver/testbench.sv
+++ Basics_Examples/Driver/testbench.sv
@@ -1,12 +1,12 @@
-`include "driver.sv"
+`include "driver.sv"      //To include the driver class
 
 module testbench();
   
-  intf vif();
+  intf vif();            //creating an interface object called as 'vif'
   
-  Driver drv;
+  Driver drv;            //creating a driver class object called as 'drv'. Note - memory won't be allocated since the new function isn't called and hence rn it is uselss
   
-  orGate OG (
+  orGate OG (            //creating an instance of the or-gate DUT
     .A(vif.A),
     .B(vif.B),
     .Y(vif.Y)
@@ -14,10 +14,12 @@ module testbench();
   
   initial
   begin
-    $monitor("A = %0b B = %0b Y = %0b" , vif.A , vif.B , vif.Y);
-    drv = new();
-    drv.main(vif);
+    $monitor("A = %0b B = %0b Y = %0b" , vif.A , vif.B , vif.Y);  //monitoring the signals
+    drv = new();                  //allocating memory in the heap for the driver class object making it actually usable
+    drv.main(vif);                //calling the driver class task 'main' to actuall do the driving part and passing the interface created
     $finish;
   end
-  
+
+  //note - it was necessary to pass the interface as an arguement in the driver here rather than just creating an interface inside the driver class because
+  //we need the interface to be connected to the dut as well and we would'nt have been able to do that if we just created an interface in the driver class and drove that
 endmodule
