6238741338d6f35e59f25bc8cb67cab145965c82
HUMUM <humum.shaikh@outlook.com>
2026-01-18 23:15:45 +0530

    Update testbench.sv

--- Basics_Examples/Monitor/testbench.sv
+++ Basics_Examples/Monitor/testbench.sv
@@ -17,8 +17,8 @@ module testbench();
   initial
   begin
     //$monitor("A = %0b B = %0b Y = %0b" , vif.A , vif.B , vif.Y);
-    drv = new();          //allocate space for the dv
-    mon = new();  
+    drv = new();          //allocate space for the driver object
+    mon = new();          //allocate space for the monitor object
     fork                  //the fork join is necessary so as to start both the tasks at the same time or else it will execute one after the other
     drv.main(vif);
     mon.main(vif);
